92dcd6a5f44de83239119ea5bfd18dfb7f6d5ced
Caleb Stevenson and Kiet Duong <kiet@>
2016-08-17 09:16:13 -0700

    add first spec: return false

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// back end
+var leapYear = function(year) {
+  return false;
+}
+
+
+// front end
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
